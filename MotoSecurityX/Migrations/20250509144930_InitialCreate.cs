7f37707e7bf0b80572d4d075963cbfdc7db685b2
caiohc28 <chcarnetti20@gmail.com>
2025-05-09 11:58:37 -0300

    Migrations e Requered adicionados

---
+++ MotoSecurityX/Migrations/20250509144930_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MotoSecurityX.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MOTO",
+                columns: table => new
+                {
+                    ID_MOTO = table.Column<int>(type: "NUMBER(10)", nullable: false)
+                        .Annotation("Oracle:Identity", "START WITH 1 INCREMENT BY 1"),
+                    PLACA = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    MODELO = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    SITUACAO = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MOTO", x => x.ID_MOTO);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MOTO");
+        }
+    }
+}
--- MotoSecurityX/Models/Moto.cs
+++ MotoSecurityX/Models/Moto.cs
@@ -1,12 +1,18 @@
-﻿namespace MotoSecurityX.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
 
+namespace MotoSecurityX.Models
+{
     public class Moto
     {
-        public int Id { get; set; } 
-        public string Placa { get; set; } = string.Empty;
-        public string Modelo { get; set; } = string.Empty;
-        public string Situacao { get; set; } = string.Empty;
-    }
+        public int Id { get; set; }
 
+        [Required(ErrorMessage = "A placa é obrigatória.")]
+        public required string Placa { get; set; }
+
+        [Required(ErrorMessage = "O modelo é obrigatório.")]
+        public required string Modelo { get; set; }
+
+        [Required(ErrorMessage = "A situação deve ser 'dentro' ou 'fora'.")]
+        public required string Situacao { get; set; }
+    }
 }
